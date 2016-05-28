5eb3aad11684791b36d2b70bec3c14d6db750f62
David Banks <dave@hoglet.com>
2016-05-28 15:29:56 +0100

    Workaround for a strange issue relating to the use of function pointers
    Change-Id: Ic58838950441cc084ed09fe33c1ad4609db1140f

--- src/tube-client.c
+++ src/tube-client.c
@@ -89,9 +89,7 @@ void kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
 
    _enable_interrupts();
 
-   if (copro >= 0 && copro < sizeof(emulator_functions) / sizeof(func_ptr)) {
-      emulator = emulator_functions[copro];
-   } else {
+   if (copro < 0 || copro >= sizeof(emulator_functions) / sizeof(func_ptr)) {
       printf("Co Pro %d has not been defined yet\r\n", copro);
       printf("Halted....\r\n");
       while (1);
@@ -101,6 +99,61 @@ void kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
 
   // Run a short set of CPU and Memory benchmarks
   benchmark();
+
+  // There is a very wierd bug on the Zero if emulator is initialized earlier
+  // It seems that putting a printf() between where emulator is initialized
+  // and used causes the 65tube emulator to become flakey on language
+  // transfer.
+  //
+  // This is *not* a code placement issue, as I had a test case where the
+  // before and after code was the same overall length. To re-create this
+  // test case, take the previous commit, and add one line before the
+  // final emulator() which is: emulator = copro_65tube_emulator();
+  //
+  // For reference, here are the diffs
+  //
+  // < = last commit
+  // > = test case described above
+  //
+  //359c359
+  //<  1f003bc:	e92d4010 	push	{r4, lr}
+  //---
+  //>  1f003bc:	e92d4038 	push	{r3, r4, r5, lr}
+  //365,368c365,368
+  //<  1f003d4:	e59f2024 	ldr	r2, [pc, #36]	; 1f00400 <kernel_main+0x44>
+  //<  1f003d8:	e59f3024 	ldr	r3, [pc, #36]	; 1f00404 <kernel_main+0x48>
+  //<  1f003dc:	e59f4024 	ldr	r4, [pc, #36]	; 1f00408 <kernel_main+0x4c>
+  //<  1f003e0:	e5921010 	ldr	r1, [r2, #16]
+  //---
+  //>  1f003d4:	e59f3024 	ldr	r3, [pc, #36]	; 1f00400 <kernel_main+0x44>
+  //>  1f003d8:	e59f4024 	ldr	r4, [pc, #36]	; 1f00404 <kernel_main+0x48>
+  //>  1f003dc:	e59f5024 	ldr	r5, [pc, #36]	; 1f00408 <kernel_main+0x4c>
+  //>  1f003e0:	e5931010 	ldr	r1, [r3, #16]
+  //370c370
+  //<  1f003e8:	e5843000 	str	r3, [r4]
+  //---
+  //>  1f003e8:	e5845000 	str	r5, [r4]
+  //373,375c373,375
+  //<  1f003f4:	e5943000 	ldr	r3, [r4]
+  //<  1f003f8:	e12fff33 	blx	r3
+  //<  1f003fc:	e8bd8010 	pop	{r4, pc}
+  //---
+  //>  1f003f4:	e5845000 	str	r5, [r4]
+  //>  1f003f8:	e8bd4038 	pop	{r3, r4, r5, lr}
+  //>  1f003fc:	ea000a95 	b	1f02e58 <copro_65tube_emulator>
+  //377,378c377,378
+  //<  1f00404:	01f02e58 	mvnseq	r2, r8, asr lr
+  //<  1f00408:	01f601a0 	mvnseq	r0, r0, lsr #3
+  //---
+  //>  1f00404:	01f601a0 	mvnseq	r0, r0, lsr #3
+  //>  1f00408:	01f02e58 	mvnseq	r2, r8, asr lr
+  //
+  // I spent a couple of hours trying to understand what's going wrong here
+  // and completely failed.
+
+  // Hence, this workaround which will probably come back and bite!
+
+  emulator = emulator_functions[copro];
 
 #ifdef HAS_MULTICORE
 
@@ -125,8 +178,4 @@ void kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
   emulator();
 
 #endif
-
-
-
-
 }
