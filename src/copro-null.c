da5a7b45814e830fa0370104d3e081b3ffa6cf90
David Banks <dave@hoglet.com>
2016-05-31 16:03:54 +0100

    Forgot to check in copro-null (oops!)
    Change-Id: Idaa797e325105409226a734d4be7c7a1bbabbac2

---
+++ src/copro-null.c
@@ -0,0 +1,39 @@
+/*
+ * Null Co Processor Emulation
+ *
+ * (c) 2016 David Banks
+ * 
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "tube-defs.h"
+#include "tube.h"
+#include "tube-ula.h"
+#include "copro-null.h"
+
+void copro_null_emulator() {
+   // Remember the current copro so we can exit if it changes
+   int last_copro = copro;
+
+   printf("This Co Pro has not been implemented yet\r\n");
+
+   // Disable the tube, so the Beeb doesn't hang
+   disable_tube();
+
+   // Wait for copro to be changed via *FX 151,230,N
+   // then exit on the next reset
+   while (1) {
+      if (tube_mailbox & ATTN_MASK) {
+         unsigned int tube_mailbox_copy = tube_mailbox;
+         tube_mailbox &= ~(ATTN_MASK | OVERRUN_MASK);
+         // With tube disabled, only writes to reg 6 are implements
+         // which update the copro global variable
+         tube_io_handler(tube_mailbox_copy);
+         // Exit on a change of copro
+         if (copro != last_copro) {
+            return;
+         }
+      }
+   }
+}
---
+++ src/copro-null.h
@@ -0,0 +1,7 @@
+// copro-null.h
+#ifndef COPRO_NULL_H
+#define COPRO_NULL_H
+
+extern void copro_null_emulator();
+
+#endif
