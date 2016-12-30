3596604bdbd33fd5efec2c407c4164c94078de9c
dp111 <dominic.plunkett@gmail.com>
2016-12-30 16:01:04 +0000

    Change release build so that for all boards except original boards coping of zip file contents is all that is required.

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -7,14 +7,15 @@ NAME=PiTubeDirect_$(date +"%Y%m%d_%H%M")_$USER
 
 for MODEL in rpi3 rpi2 rpi rpibplus rpizero 
 do    
-    DIR=releases/${NAME}/${MODEL}
+    DIR=releases/${NAME}
     mkdir -p ${DIR}
     ./clobber.sh
     ./configure_${MODEL}.sh
-    make -B
-    cp kernel*.img ${DIR}
+    make -B -j
+    mv kernel*.img ${DIR}/kernel${MODEL}.img
 done
 
+cp config.txt ${DIR}/config.txt
 cd releases
 zip -qr ${NAME}.zip ${NAME}
 cd ..
