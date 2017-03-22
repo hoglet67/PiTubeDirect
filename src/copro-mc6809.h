7cf89363490116e5cc51e8d1023378c198ef51a1
dp111 <dominic.plunkett@gmail.com>
2017-03-22 18:47:19 +0000

    remove old 6809 cores

--- src/copro-mc6809.h
+++
@@ -1,9 +0,0 @@
-// copro-mc6809.h
-#ifndef COPRO_MC6809_H
-#define COPRO_MC6809_H
-
-#define USE_HD6309
-
-extern void copro_mc6809_emulator();
-
-#endif
--- src/copro-mc6809sc.h
+++
@@ -1,7 +0,0 @@
-// copro-mc6809sc.h
-#ifndef COPRO_MC6809SC_H
-#define COPRO_MC6809SC_H
-
-extern void copro_mc6809sc_emulator();
-
-#endif
