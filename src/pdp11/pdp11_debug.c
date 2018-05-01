751dca0f9988a79da358e205f509b613062bb8dc
David Banks <dave@hoglet.com>
2018-05-01 11:51:31 +0100

    PDP-11 Co Pro - disassembler improvements
    Change-Id: Ice98b9353affecdaf5da6fdb9e0ce5c15ff70939

--- src/pdp11/pdp11_debug.c
+++ src/pdp11/pdp11_debug.c
@@ -43,8 +43,8 @@ static const char *dbg_reg_names[] = {
    "SP",
    "PC",
    "PS",
-   "PU",
-   "CU",
+   "PREVUSER",
+   "CURRUSER",
    "INTQUEUE",
    NULL
 };
@@ -113,18 +113,18 @@ static size_t dbg_reg_print(int which, char *buf, size_t bufsize) {
       char c;
       const char *flagnameptr = flagname;
       int psr = dbg_reg_get(i_PS);
-      
+
       if (bufsize < 200) {
          strncpy(buf, "buffer too small!!!", bufsize);
       }
-      
+
       // Print the I-bit I field
       *buf++ = 'I';
       *buf++ = ':';
       sprintf(buf, "%x", (psr >> 5) & 0x07);
       buf++;
       *buf++ = ' ';
-      
+
       // Print the remaining flag bits
       bit = 0x10;
       for (i = 0; i < 5; i++) {
@@ -197,123 +197,145 @@ typedef struct {
 enum { DD = 1 << 1, S = 1 << 2, RR = 1 << 3, O = 1 << 4, N = 1 << 5 };
 
 static D disamtable[] = {
-   { 0077700, 0005000, "CLR", DD, true },
-   { 0077700, 0005100, "COM", DD, true },
-   { 0077700, 0005200, "INC", DD, true },
-   { 0077700, 0005300, "DEC", DD, true },
-   { 0077700, 0005400, "NEG", DD, true },
-   { 0077700, 0005700, "TST", DD, true },
-   { 0077700, 0006200, "ASR", DD, true },
-   { 0077700, 0006300, "ASL", DD, true },
-   { 0077700, 0006000, "ROR", DD, true },
-   { 0077700, 0006100, "ROL", DD, true },
-   { 0177700, 0000300, "SWAB", DD, false },
-   { 0077700, 0005500, "ADC", DD, true },
-   { 0077700, 0005600, "SBC", DD, true },
-   { 0177700, 0006700, "SXT", DD, false },
-   { 0070000, 0010000, "MOV", S | DD, true },
-   { 0070000, 0020000, "CMP", S | DD, true },
-   { 0170000, 0060000, "ADD", S | DD, false },
-   { 0170000, 0160000, "SUB", S | DD, false },
-   { 0070000, 0030000, "BIT", S | DD, true },
-   { 0070000, 0040000, "BIC", S | DD, true },
-   { 0070000, 0050000, "BIS", S | DD, true },
-   { 0177000, 0070000, "MUL", RR | DD, false },
-   { 0177000, 0071000, "DIV", RR | DD, false },
-   { 0177000, 0072000, "ASH", RR | DD, false },
+   // One-operand instructions
+   { 0077700, 0005000, "CLR",       DD,  true },
+   { 0077700, 0005100, "COM",       DD,  true },
+   { 0077700, 0005200, "INC",       DD,  true },
+   { 0077700, 0005300, "DEC",       DD,  true },
+   { 0077700, 0005400, "NEG",       DD,  true },
+   { 0077700, 0005500, "ADC",       DD,  true },
+   { 0077700, 0005600, "SBC",       DD,  true },
+   { 0077700, 0005700, "TST",       DD,  true },
+   { 0077700, 0006000, "ROR",       DD,  true },
+   { 0077700, 0006100, "ROL",       DD,  true },
+   { 0077700, 0006200, "ASR",       DD,  true },
+   { 0077700, 0006300, "ASL",       DD,  true },
+   { 0177700, 0006700, "SXT",       DD, false },
+   { 0177700, 0000300, "SWAB",      DD, false },
+   // One-and-a-half-operand instructions
+   { 0177000, 0070000, "MUL",  RR | DD, false },
+   { 0177000, 0071000, "DIV",  RR | DD, false },
+   { 0177000, 0072000, "ASH",  RR | DD, false },
    { 0177000, 0073000, "ASHC", RR | DD, false },
-   { 0177400, 0000400, "BR", O, false },
-   { 0177400, 0001000, "BNE", O, false },
-   { 0177400, 0001400, "BEQ", O, false },
-   { 0177400, 0100000, "BPL", O, false },
-   { 0177400, 0100400, "BMI", O, false },
-   { 0177400, 0101000, "BHI", O, false },
-   { 0177400, 0101400, "BLOS", O, false },
-   { 0177400, 0102000, "BVC", O, false },
-   { 0177400, 0102400, "BVS", O, false },
-   { 0177400, 0103000, "BCC", O, false },
-   { 0177400, 0103400, "BCS", O, false },
-   { 0177400, 0002000, "BGE", O, false },
-   { 0177400, 0002400, "BLT", O, false },
-   { 0177400, 0003000, "BGT", O, false },
-   { 0177400, 0003400, "BLE", O, false },
-   { 0177700, 0000100, "JMP", DD, false },
-   { 0177000, 0004000, "JSR", RR | DD, false },
-   { 0177770, 0000200, "RTS", RR, false },
-   { 0177777, 0006400, "MARK", 0, false },
-   { 0177000, 0077000, "SOB", RR | O, false },
-   { 0177777, 0000005, "RESET", 0, false },
-   { 0177700, 0006500, "MFPI", DD, false },
-   { 0177700, 0006600, "MTPI", DD, false },
-   { 0177777, 0000001, "WAIT", 0, false },
-   { 0177777, 0000002, "RTI", 0, false },
-   { 0177777, 0000006, "RTT", 0, false },
-   { 0177400, 0104000, "EMT", N, false },
-   { 0177400, 0104400, "TRAP", N, false },
-   { 0177777, 0000003, "BPT", 0, false },
-   { 0177777, 0000004, "IOT", 0, false },
+   { 0177000, 0074000, "XOR",  RR | DD, false },
+   // Two-operand instructions
+   { 0070000, 0010000, "MOV",   S | DD,  true },
+   { 0070000, 0020000, "CMP",   S | DD,  true },
+   { 0070000, 0030000, "BIT",   S | DD,  true },
+   { 0070000, 0040000, "BIC",   S | DD,  true },
+   { 0070000, 0050000, "BIS",   S | DD,  true },
+   { 0170000, 0060000, "ADD",   S | DD, false },
+   { 0170000, 0160000, "SUB",   S | DD, false },
+   // Branch instructions
+   { 0177400, 0000400, "BR ",        O, false },
+   { 0177400, 0001000, "BNE",        O, false },
+   { 0177400, 0001400, "BEQ",        O, false },
+   { 0177400, 0002000, "BGE",        O, false },
+   { 0177400, 0002400, "BLT",        O, false },
+   { 0177400, 0003000, "BGT",        O, false },
+   { 0177400, 0003400, "BLE",        O, false },
+   { 0177400, 0100000, "BPL",        O, false },
+   { 0177400, 0100400, "BMI",        O, false },
+   { 0177400, 0101000, "BHI",        O, false },
+   { 0177400, 0101400, "BLOS",       O, false },
+   { 0177400, 0102000, "BVC",        O, false },
+   { 0177400, 0102400, "BVS",        O, false },
+   { 0177400, 0103000, "BCC",        O, false },
+   { 0177400, 0103400, "BCS",        O, false },
+   // Other instructions
+   { 0177700, 0000100, "JMP",       DD, false },
+   { 0177000, 0077000, "SOB",  RR |  O, false },
+   { 0177000, 0004000, "JSR",  RR | DD, false },
+   { 0177770, 0000200, "RTS",  RR     , false },
+   { 0177777, 0000002, "RTI",        0, false },
+   { 0177400, 0104400, "TRAP",       N, false },
+   { 0177400, 0104000, "EMT",        N, false },
+   { 0177777, 0000003, "BPT",        0, false },
+   { 0177777, 0000004, "IOT",        0, false },
+   { 0177777, 0000006, "RTT",        0, false },
+   // Zero-operand instructions
+   { 0177777, 0000000, "HALT",       0, false },
+   { 0177777, 0000001, "WAIT",       0, false },
+   { 0177777, 0000005, "RESET",      0, false },
+   { 0177777, 0000240, "NOP",        0, false },
+   // Processor Status Word
+   { 0177770, 0000230, "SPL",        0, false },
+   { 0177777, 0000241, "CLC",        0, false },
+   { 0177777, 0000242, "CLV",        0, false },
+   { 0177777, 0000244, "CLZ",        0, false },
+   { 0177777, 0000250, "CLN",        0, false },
+   { 0177777, 0000257, "CCC",        0, false },
+   { 0177777, 0000261, "SEC",        0, false },
+   { 0177777, 0000262, "SEV",        0, false },
+   { 0177777, 0000264, "SEZ",        0, false },
+   { 0177777, 0000270, "SEN",        0, false },
+   { 0177777, 0000277, "SCC",        0, false },
+   // Other
+   { 0177777, 0006400, "MARK",       0, false },
+   { 0177700, 0006500, "MFPI",      DD, false },
+   { 0177700, 0006600, "MTPI",      DD, false },
+   // Terminator
    { 0, 0, "", 0, false }, };
 
 static uint16_t read16(uint16_t a) {
    return copro_pdp11_read16(a);
 }
 
-static int disasmaddr(char **bufp, uint16_t m, uint16_t a) {
-   if (m & 7) {
+
+// Returns the number of additional operand bytes for the addressing mode
+static int disaslen(uint16_t m) {
+   if (((m & 027) == 027) || ((m & 060) == 060)) {
+      // ((m & 027) == 027) catches 027 037 067 077
+      // ((m & 060) == 060) catches 06x 07x
+      return 2;
+   } else {
+      return 0;
+   }
+}
+
+static int disasmaddr(char *buf, uint16_t m, uint16_t a) {
+   if ((m & 7) == 7) {
       switch (m) {
       case 027:
-         a += 2;
-         *bufp += sprintf(*bufp, "#%06o", read16(a));
-         return a;
+         return sprintf(buf, "#%06o", read16(a));
       case 037:
-         a += 2;
-         *bufp += sprintf(*bufp, "@#%06o", read16(a));
-         return a;
+         return sprintf(buf, "@#%06o", read16(a));
       case 067:
-         a += 2;
-         *bufp += sprintf(*bufp, "*%06o", (a + 2 + (read16(a))) & 0xFFFF);
-         return a;
+         return sprintf(buf, "%06o", (a + 2 + (read16(a))) & 0xFFFF);
       case 077:
-         *bufp += sprintf(*bufp, "**%06o", (a + 2 + (read16(a))) & 0xFFFF);
-         return a;
+         return sprintf(buf, "@%06o", (a + 2 + (read16(a))) & 0xFFFF);
       }
    }
-   
+
    switch (m & 070) {
    case 000:
-      *bufp += sprintf(*bufp, "%s", dbg_reg_names[m & 7]);
-      break;
+      return sprintf(buf, "%s", dbg_reg_names[m & 7]);
    case 010:
-      *bufp += sprintf(*bufp, "(%s)", dbg_reg_names[m & 7]);
-      break;
+      return sprintf(buf, "(%s)", dbg_reg_names[m & 7]);
    case 020:
-      *bufp += sprintf(*bufp, "(%s)+", dbg_reg_names[m & 7]);
-      break;
+      return sprintf(buf, "(%s)+", dbg_reg_names[m & 7]);
    case 030:
-      *bufp += sprintf(*bufp, "*(%s)+", dbg_reg_names[m & 7]);
-      break;
+      return sprintf(buf, "@(%s)+", dbg_reg_names[m & 7]);
    case 040:
-      *bufp += sprintf(*bufp, "-(%s)", dbg_reg_names[m & 7]);
-      break;
+      return sprintf(buf, "-(%s)", dbg_reg_names[m & 7]);
    case 050:
-      *bufp += sprintf(*bufp, "*-(%s)", dbg_reg_names[m & 7]);
-      break;
+      return sprintf(buf, "@-(%s)", dbg_reg_names[m & 7]);
    case 060:
-      a += 2;
-      *bufp += sprintf(*bufp, "%06o(%s)", read16(a), dbg_reg_names[m & 7]);
-      break;
+      return sprintf(buf, "%06o(%s)", read16(a), dbg_reg_names[m & 7]);
    case 070:
-      a += 2;
-      *bufp += sprintf(*bufp, "*%06o(%s)", read16(a), dbg_reg_names[m & 7]);
-      break;
-   }   
-   return a;
+      return sprintf(buf, "@%06o(%s)", read16(a), dbg_reg_names[m & 7]);
+   }
+
+   return 0;
 }
 
 static uint16_t disasm(char *buf, uint16_t a) {
    uint16_t ins;
+
+   // Fetch the instruction
    ins = read16(a);
-   buf += sprintf(buf, "%06o %06o : ", a, ins);   
+
+   // Lookup the instruction in table
    D l = disamtable[(sizeof(disamtable) / sizeof(disamtable[0])) - 1];
    uint8_t i;
    for (i = 0; disamtable[i].inst; i++) {
@@ -322,42 +344,87 @@ static uint16_t disasm(char *buf, uint16_t a) {
          break;
       }
    }
-   if (l.inst == 0) {
+
+   // Decode the instruction
+   uint16_t s = (ins & 07700) >> 6;
+   uint16_t d = ins & 077;
+   uint8_t  o = ins & 0377;
+
+   // Work out the lenth
+   int len = 2;
+   if (l.flag & S) {
+      len += disaslen(s);
+   }
+   if (l.flag & DD) {
+      len += disaslen(d);
+   }
+
+   // Ouput the address
+   buf += sprintf(buf, "%06o : ", a);
+
+   // Output the instruction, max len is 3 words
+   for (int i = 0; i < 6; i += 2) {
+      if (i < len) {
+         buf += sprintf(buf, "%06o ", read16(a + i));
+      } else {
+         buf += sprintf(buf, "       ");
+      }
+   }
+   buf += sprintf(buf, ": ");
+
+   // Consume the instruction
+   a += 2;
+
+   // Output the menmonic
+   if (disamtable[i].inst == 0) {
       sprintf(buf, "???");
-      return a + 2;
+      return a;
    }
    buf += sprintf(buf, l.msg);
+
+   // Output the byte mode qualifier
    if (l.b && (ins & 0100000)) {
       buf += sprintf(buf, "B");
    }
-   uint16_t s = (ins & 07700) >> 6;
-   uint16_t d = ins & 077;
-   uint8_t o = ins & 0377;
+
+   // Output the operands
    switch (l.flag) {
    case S | DD:
       buf += sprintf(buf, " ");
-      a = disasmaddr(&buf, s, a);
+      buf += disasmaddr(buf, s, a);
       buf += sprintf(buf, ",");
+      // Consume the operand (if there us one)
+      a   += disaslen(s);
+      // Fall thrrough to DD
    case DD:
       buf += sprintf(buf, " ");
-      a = disasmaddr(&buf, d, a);
+      buf += disasmaddr(buf, d, a);
+      // Consume the operand (if there us one)
+      a   += disaslen(d);
       break;
    case RR | O:
       buf += sprintf(buf, " %s,", dbg_reg_names[(ins & 0700) >> 6]);
       o &= 077;
+      // fall through to O
    case O:
       if (o & 0x80) {
-         buf += sprintf(buf, " -%03o", (2 * ((0xFF ^ o) + 1)));
+         buf += sprintf(buf, " %06o", (a - (2 * ((0xFF ^ o) + 1))) & 0xffff);
       } else {
-         buf += sprintf(buf, " +%03o", (2 * o));
+         buf += sprintf(buf, " %06o", (a + (2 * o)) & 0xffff);
       };
       break;
    case RR | DD:
       buf += sprintf(buf, " %s, ", dbg_reg_names[(ins & 0700) >> 6]);
-      a = disasmaddr(&buf, d, a);
+      buf += disasmaddr(buf, d, a);
+      // Consume the operand (if there us one)
+      a   += disaslen(d);
+      break;
    case RR:
       buf += sprintf(buf, " %s", dbg_reg_names[ins & 7]);
+      break;
+   case N:
+      buf += sprintf(buf, " %d", ins & 0xFF);
+      break;
    }
-   return a + 2;
+   return a;
 }
-
