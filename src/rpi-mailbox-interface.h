101323ab4993534c61fc27760f8297d5843cba68
David Banks <dave@hoglet.com>
2020-04-23 10:16:09 +0100

    Mailbox: Add debug versions of PropertyProcess()
    Change-Id: I69492ae197125791da3aaa82f97fdf124fa4a28f

--- src/rpi-mailbox-interface.h
+++ src/rpi-mailbox-interface.h
@@ -146,6 +146,8 @@ extern void RPI_PropertyInit( void );
 extern void RPI_PropertyAddTag( rpi_mailbox_tag_t tag, ... );
 extern int RPI_PropertyProcess( void );
 extern void RPI_PropertyProcessNoCheck( void );
+extern int RPI_PropertyProcessDebug( void );
+extern void RPI_PropertyProcessNoCheckDebug( void );
 extern rpi_mailbox_property_t* RPI_PropertyGet( rpi_mailbox_tag_t tag );
 
 #endif
