e46641464c833d5f3a6b0893b2b4d78eb472e5a7
revaldinho <richevans@mac.com>
2020-04-25 10:48:33 +0100

    F100: add simple makeroms.sh to create the tuberom.c directly from hex file

---
+++ src/f100/makeroms.sh
@@ -0,0 +1,13 @@
+# Need to setup your path and PYTHONPATH to the F100Asm
+
+F100Asm.py -f tuberom.S -g hex16 -o tuberom.raw -g hex16 | tee tuberom.lst
+
+awk '\
+    /^0x0780:/{print("#include \"tuberom.h\"\nuint16_t tuberom_f100[0x800] = {")}\
+    /^0x08..:|^0x09..:|^0x0A..:|^0x0B..:/{print($2)}\
+    /^0x7E80:/{print("};\nuint16_t tuberom_f100_high[0x100] = {")}\
+    /^0x7F..:/{print($2)} \
+    /^0x8000:/{print("};")}' tuberom.raw  > tuberom.c
+
+
+
