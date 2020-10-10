2c37b1325fbddb3caab0663e1233ac32670265c3
dp111 <dp111@users.noreply.github.com>
2020-10-10 09:41:33 +0100

    RPI3 doesn't have crypto extensions

--- src/toolchain-arm-none-eabi-rpi3.cmake
+++ src/toolchain-arm-none-eabi-rpi3.cmake
@@ -65,7 +65,7 @@ set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfloat-abi=hard" )
 
 set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a" )
 set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mtune=cortex-a53" )
-set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=crypto-neon-fp-armv8" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=neon-fp-armv8" )
 
 set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
 set( CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
