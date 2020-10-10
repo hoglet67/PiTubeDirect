906b30d07eceae89408e0e3497da3e21cf002495
dp111 <19616418+dp111@users.noreply.github.com>
2020-10-10 13:28:01 +0100

    Only include the header file once

--- src/yaze/mem_mmu.h
+++ src/yaze/mem_mmu.h
@@ -16,6 +16,8 @@ General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
+#ifndef MEM_MMU_H
+#define MEM_MMU_H
 
 #include <limits.h>
 
@@ -243,3 +245,4 @@ void initMEM();
 void initMMU();
 void loadMMU();
 void printMMU();
+#endif
