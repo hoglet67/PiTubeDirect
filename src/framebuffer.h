a0f5d932bbd7edf341b7be53aa383a982589aca7
David Banks <dave@hoglet.com>
2021-01-09 12:26:10 +0000

    Frame Buffer: port old atom_vdu code to current release
    Change-Id: I5067f9f05df8afece42f8099bcda6f4e2c5f3ac9

---
+++ src/framebuffer.h
@@ -0,0 +1,28 @@
+// framebuffer.h
+
+#ifndef FRAMEBUFFER_H
+#define FRAMEBUFFER_H
+
+void fb_emulator_init();
+
+void fb_initialize();
+
+void fb_writec(int c);
+
+void fb_writes(char *string);
+
+void fb_putpixel(int x, int y, unsigned int color);
+
+void fb_draw_line(int x, int y, int x2, int y2, unsigned int color);
+
+void fb_fill_triangle(int x, int y, int x2, int y2, int x3, int y3, unsigned int color);
+
+uint32_t fb_get_address();
+
+uint32_t fb_get_width();
+
+uint32_t fb_get_height();
+
+uint32_t fb_get_bpp32();
+
+#endif
