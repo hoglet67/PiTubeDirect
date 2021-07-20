6a2d59b25c05420d5b855188987395d81bf67510
dp111 <19616418+dp111@users.noreply.github.com>
2021-07-20 23:57:06 +0100

    Coverity wants the return value checked

--- src/debugger/linenoise.c
+++ src/debugger/linenoise.c
@@ -1260,7 +1260,7 @@ int linenoiseHistorySave(const char *filename) {
     fp = fopen(filename,"w");
     umask(old_umask);
     if (fp == NULL) return -1;
-    chmod(filename,S_IRUSR|S_IWUSR);
+    if (chmod(filename,S_IRUSR|S_IWUSR)) return -1;
     for (j = 0; j < history_len; j++)
         fprintf(fp,"%s\n",history[j]);
     fclose(fp);
