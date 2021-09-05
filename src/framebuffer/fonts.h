46f7e440a7aba50afcbc3308bc02c9596a20ca7f
dp111 <19616418+dp111@users.noreply.github.com>
2021-09-05 13:21:14 +0100

    white space

--- src/framebuffer/fonts.h
+++ src/framebuffer/fonts.h
@@ -9,7 +9,7 @@
 
 typedef struct font {
    // The raw font data itself
-   const char *name;         // (max) 8 character ASCII name of the font
+   const char *name;   // (max) 8 character ASCII name of the font
    uint8_t *data;      // pointer to the raw font data
    int bytes_per_char; // Number of bytes of raw data per character
    int num_chars;      // Number of characters in the character set
@@ -37,15 +37,15 @@ typedef struct font {
    void    (*set_scale_h)(struct font *font, int scale_h);
    void   (*set_rounding)(struct font *font, int rounding);
 
-   const char *     (*get_name)(struct font *font);
-   uint32_t (*get_number)(struct font *font);
-   int   (*get_spacing_w)(struct font *font);
-   int   (*get_spacing_h)(struct font *font);
-   int     (*get_scale_w)(struct font *font);
-   int     (*get_scale_h)(struct font *font);
-   int    (*get_rounding)(struct font *font);
-   int   (*get_overall_w)(struct font *font);
-   int   (*get_overall_h)(struct font *font);
+   const char * (*get_name)(struct font *font);
+   uint32_t  (*get_number)(struct font *font);
+   int    (*get_spacing_w)(struct font *font);
+   int    (*get_spacing_h)(struct font *font);
+   int      (*get_scale_w)(struct font *font);
+   int      (*get_scale_h)(struct font *font);
+   int     (*get_rounding)(struct font *font);
+   int    (*get_overall_w)(struct font *font);
+   int    (*get_overall_h)(struct font *font);
 
    void     (*write_char)(struct font *font, screen_mode_t *screen, int c, int x, int y, pixel_t fg_col, pixel_t bg_col);
    int      ( *read_char)(struct font *font, screen_mode_t *screen, int x, int y,                        pixel_t bg_col);
