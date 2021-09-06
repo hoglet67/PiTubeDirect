30807ae535036b579d4bf00c85e32ebd8a502006
David Banks <dave@hoglet.com>
2021-09-06 14:34:11 +0100

    Pi 4: Fix an ARM Timer bug that resulted in 45x the expected IRQ rate
    Change-Id: I7b294d1ed270fce90ea4f1ff299e2396176e6d99

--- src/rpi-armtimer.c
+++ src/rpi-armtimer.c
@@ -15,11 +15,21 @@ void RPI_ArmTimerInit(void)
 {
    _data_memory_barrier();
 
+   // The Pi 0 and 3A PreDivider defaults to 399, which gives a
+   // timer base frequency of 400 / (399 + 1) = 1MHz
+   //
+   // The Pi 4B PreDivider defaults to 10, which gives a
+   // timer base frequency of 500 / (10 + 1) = 45.45MHz.
+   //
+   // So we need to change the value on the Pi 4B:
+#ifdef RPI4
+   rpiArmTimer->PreDivider = 499;
+#endif
+
    // Timer defaults to running at ~1MHz
 
    rpiArmTimer->Load = 1000;
    rpiArmTimer->Control = RPI_ARMTIMER_CTRL_PRESCALE_1 | RPI_ARMTIMER_CTRL_INT_ENABLE | RPI_ARMTIMER_CTRL_ENABLE;
 
    _data_memory_barrier();
-
 }
