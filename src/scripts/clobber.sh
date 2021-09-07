d2aaa142e1063190298d6480bf25111963da2613
dp111 <19616418+dp111@users.noreply.github.com>
2021-09-07 19:15:55 +0100

    Rejig kernel names to be closer to the default names and tidy up config.txt

--- src/scripts/clobber.sh
+++ src/scripts/clobber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf CMakeFiles/ CMakeCache.txt  cmake_install.cmake kernel.img kernel7.img Makefile tube-client tube-client.cbp gitversion.h
+rm -rf CMakeFiles/ CMakeCache.txt  cmake_install.cmake kernel.img kernel7.img kernel8-32.img kernelrpi4.img Makefile tube-client tube-client.cbp gitversion.h
 
