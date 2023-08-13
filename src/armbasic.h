hoglet67/PiTubeDirect

commit c0d15e04ec184a57b4c09121f6b7ef081beab263

place arm basic with reference to the start of code

src/armbasic.h
@@ -11,8 +11,8 @@
 // transfer.
 
 // #define BAS135H_ROM
-
-#define ARM_BASIC_START (0x0C800000)
+extern unsigned int _start ;
+#define ARM_BASIC_START (_start-0x00C00000)
 
 // Note, the two version have slightly different start addresses
 #ifdef BAS135H_ROM

commit 7343da1a82dc1167822335cbb87b3112f812fb61

Native ARM Co Pro: Fix *ARMBASIC crash (introduced in c0d15e0)
Change-Id: I92fd176a4aaff3177bf8b3cda7ca7a5d2a8a3670

src/armbasic.h
@@ -11,8 +11,9 @@
 // transfer.
 
 // #define BAS135H_ROM
-extern unsigned int _start ;
-#define ARM_BASIC_START (_start-0x00C00000)
+
+extern int _start();
+#define ARM_BASIC_START (((unsigned int)_start)-0x00C00000)
 
 // Note, the two version have slightly different start addresses
 #ifdef BAS135H_ROM