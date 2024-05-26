15d193fe1d4b6006e52166eb639459edc7436942
David Banks <dave@hoglet.com>
2024-05-26 13:05:59 +0100

    Updated the release name to Indigo-alpha8
    Change-Id: I888815970f152aad85807210b28b8081ed0688b8

--- src/tube-defs.h
+++ src/tube-defs.h
@@ -3,7 +3,7 @@
 #ifndef TUBE_DEFS_H
 #define TUBE_DEFS_H
 
-#define RELEASENAME "Indigo-alpha7"
+#define RELEASENAME "Indigo-alpha8"
 
 // If defined, the doorbell is used rather then the mailbox
 #define USE_DOORBELL
